64b4e26bb0f3b99b759c24445337c614e1872817
Bradley Sakdol <1362081+bsakdol@users.noreply.github.com>
2022-06-24 14:35:16 -0600

    add conditional null to all outputs

--- modules/vpc-endpoints/outputs.tf
+++ modules/vpc-endpoints/outputs.tf
@@ -3,7 +3,7 @@
 ################################################################################
 output "arns" {
   description = "The Amazon Resource Name (ARN) of the VPC endpoint."
-  value       = try(aws_vpc_endpoint.this[*].arn)
+  value       = try(aws_vpc_endpoint.this[*].arn, null)
 }
 
 output "cidr_blocks" {
@@ -11,17 +11,17 @@ output "cidr_blocks" {
     The list of CIDR blocks for the exposed AWS service. Applicable for
     endpoints of type Gateway.
   EOT
-  value       = try(aws_vpc_endpoint.this[*].cidr_blocks)
+  value       = try(aws_vpc_endpoint.this[*].cidr_blocks, null)
 }
 
 output "dns_entries" {
   description = "The DNS entries for the VPC Endpoints. Applicable for endpoints of type Interface."
-  value       = try(aws_vpc_endpoint.this[*].dns_entry)
+  value       = try(aws_vpc_endpoint.this[*].dns_entry, null)
 }
 
 output "ids" {
   description = "The ID of the VPC endpoints."
-  value       = try(aws_vpc_endpoint.this[*].id)
+  value       = try(aws_vpc_endpoint.this[*].id, null)
 }
 
 output "network_interface_ids" {
@@ -29,12 +29,12 @@ output "network_interface_ids" {
     One or more network interfaces for the VPC Endpoints. Applicable for
     endpoints of type Interface.
   EOT
-  value       = try(aws_vpc_endpoint.this[*].network_interface_ids)
+  value       = try(aws_vpc_endpoint.this[*].network_interface_ids, null)
 }
 
 output "owner_ids" {
   description = "The ID of the AWS account that owns the VPC endpoints."
-  value       = try(aws_vpc_endpoint.this[*].owner_id)
+  value       = try(aws_vpc_endpoint.this[*].owner_id, null)
 }
 
 output "prefix_list_ids" {
@@ -42,17 +42,17 @@ output "prefix_list_ids" {
     The prefix list ID of the exposed AWS service. Applicable for endpoints of
     type Gateway.
   EOT
-  value       = try(aws_vpc_endpoint.this[*].prefix_list_id)
+  value       = try(aws_vpc_endpoint.this[*].prefix_list_id, null)
 }
 
 output "requestor_managed" {
   description = "Whether or not the VPC Endpoint is being managed by its service."
-  value       = try(aws_vpc_endpoint.this[*].requestor_managed)
+  value       = try(aws_vpc_endpoint.this[*].requestor_managed, null)
 }
 
 output "states" {
   description = "The state of the VPC endpoint."
-  value       = try(aws_vpc_endpoint.this[*].state)
+  value       = try(aws_vpc_endpoint.this[*].state, null)
 }
 
 output "tags_all" {
@@ -60,7 +60,7 @@ output "tags_all" {
     A map of tags assigned to the VPC Endpoints, including those inherited from
     the provider `default_tags` configuration block.
   EOT
-  value       = try(aws_vpc_endpoint.this[*].tags_all)
+  value       = try(aws_vpc_endpoint.this[*].tags_all, null)
 }
 
 output "vpc_endpoints_all" {
