66a049ce4dfe7f6cf4e77bc1560c3e41d0789880
Bradley Sakdol <1362081+bsakdol@users.noreply.github.com>
2022-11-14 11:55:41 -0700

    refactor: remove deprecated classiclink options
    Terraform has deprecated classiclink attributes
    - Classiclink is no longer supported by this module

--- main.tf
+++ main.tf
@@ -28,12 +28,10 @@ locals {
 resource "aws_vpc" "this" {
   count = var.manage_vpc ? 1 : 0
 
-  cidr_block                     = var.cidr_block
-  enable_classiclink             = var.enable_classiclink
-  enable_classiclink_dns_support = var.enable_classiclink_dns_support
-  enable_dns_hostnames           = var.enable_dns_hostnames
-  enable_dns_support             = var.enable_dns_support
-  instance_tenancy               = var.instance_tenancy
+  cidr_block           = var.cidr_block
+  enable_dns_hostnames = var.enable_dns_hostnames
+  enable_dns_support   = var.enable_dns_support
+  instance_tenancy     = var.instance_tenancy
 
   tags = merge(
     var.tags,
